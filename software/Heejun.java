5f4b86ff4a0214c6716641ae09c78202b1e3452b
cokacider <quwjrajfl@naver.com>
2018-03-07 11:39:49 +0900

    heejun test

---
+++ software/Heejun.java
@@ -0,0 +1,8 @@
+public class Heejun {
+    public static void main(String[] arguments) {
+        String foo = "Heejun"; // "자바"라는 문자열을 왼쪽의 foo 변수에 대입
+        System.out.println(foo);
+        foo = "Lee"; // "프로그래밍"이라는 문자열을 왼쪽의 foo 변수에 대입
+        System.out.println(foo);
+    }
+}
